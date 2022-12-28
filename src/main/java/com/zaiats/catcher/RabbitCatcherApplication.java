17cb3bd6bae8705035433271097372c47647f8c1
Olesia Zaiats <olesia.zayats@gmail.com>
2022-12-27 21:04:24 -0600

    Initial commit

---
+++ src/main/java/com/zaiats/catcher/RabbitCatcherApplication.java
@@ -0,0 +1,13 @@
+package com.zaiats.catcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitCatcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitCatcherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zaiats/catcher/RabbitCatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zaiats.catcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitCatcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
