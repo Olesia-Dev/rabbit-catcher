b7559439e4dbac8a7c7d24ae7f2a2b39a8f5026b
Olesia Zaiats <olesia.zayats@gmail.com>
2023-05-08 18:51:02 -0500

    Initial Data creation

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO candidates (first_name, last_name, email, current_position) VALUES
+  ('Pavlo', 'Savka', 'pavlo.savka@mail.com', 'Python Developer'),
+  ('Lesia', 'Yak', 'lesia.yak@mail.com', 'Account Manager'),
+  ('Vasyl', 'Pyrig', 'vasyl.pyrig@mail.com', 'Business Manager');
