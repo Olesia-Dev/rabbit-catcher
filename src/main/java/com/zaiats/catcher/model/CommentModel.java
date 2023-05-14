6eece79a5c29049c0ab88ab407fcbff15c668fc6
Olesia Zaiats <olesia.zayats@gmail.com>
2023-05-14 13:41:30 -0500

    Create and read comment

--- src/main/java/com/zaiats/catcher/model/CommentModel.java
+++ src/main/java/com/zaiats/catcher/model/CommentModel.java
@@ -1,10 +1,12 @@
 package com.zaiats.catcher.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.zaiats.catcher.repository.entity.Comment;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 import java.time.Instant;
+import java.util.Objects;
 
 import static com.fasterxml.jackson.annotation.JsonProperty.Access.READ_ONLY;
 
@@ -21,6 +23,80 @@ public class CommentModel {
     @Size(max = 64, message = "{xxx}")
     private String author;
 
-    private Instant timestamp;
+    @JsonProperty(access = READ_ONLY)
+    private Instant timestamp = Instant.now();
+
+    public CommentModel() {
+    }
+
+    public CommentModel(Long id, String text, String author, Instant timestamp) {
+        this.id = id;
+        this.text = text;
+        this.author = author;
+        this.timestamp = timestamp;
+    }
+
+    public static CommentModel fromEntity(Comment comment) {
+        return new CommentModel(
+                comment.getId(),
+                comment.getText(),
+                comment.getAuthor(),
+                comment.getTimestamp());
+    }
+
+    public static Comment toEntity(CommentModel commentModel) {
+        Comment comment = new Comment();
+        comment.setText(commentModel.getText());
+        comment.setAuthor(commentModel.getAuthor());
+        comment.setTimestamp(commentModel.getTimestamp());
+        return comment;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Instant getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Instant timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof CommentModel that)) return false;
+        return Objects.equals(id, that.id)
+                && Objects.equals(text, that.text)
+                && Objects.equals(author, that.author)
+                && Objects.equals(timestamp, that.timestamp);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, text, author, timestamp);
+    }
 
 }
--- src/main/java/com/zaiats/catcher/service/CommentServiceImpl.java
+++ src/main/java/com/zaiats/catcher/service/CommentServiceImpl.java
@@ -1,5 +1,29 @@
 package com.zaiats.catcher.service;
 
+import com.zaiats.catcher.exception.ResourceNotFoundException;
+import com.zaiats.catcher.model.CommentModel;
+import com.zaiats.catcher.repository.CommentRepository;
+import com.zaiats.catcher.repository.entity.Comment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CommentServiceImpl implements CommentService {
+
+    @Autowired
+    private CommentRepository commentRepository;
+
+    @Override
+    public CommentModel getCommentById(Long id) {
+        Comment foundComment = commentRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Comment with ID: " + id + " not found!"));
+        return CommentModel.fromEntity(foundComment);
+    }
+
+    @Override
+    public CommentModel saveComment(CommentModel commentModel) {
+        Comment comment = CommentModel.toEntity(commentModel);
+        return CommentModel.fromEntity(commentRepository.save(comment));
+    }
 
 }
